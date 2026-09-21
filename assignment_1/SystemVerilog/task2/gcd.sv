26ff9f564d8601567051477de6f2771563e5d491
PC <mfedorczyk@proton.me>
2026-09-21 17:33:05 +0200

    Added Combinatorial logic to the gcd.sv file. Initial version.

--- assignment_1/SystemVerilog/task2/gcd.sv
+++ assignment_1/SystemVerilog/task2/gcd.sv
@@ -11,7 +11,6 @@
 //
 // -----------------------------------------------------------------------------
 
-
 module gcd (
     input  logic          clk,    // The clock signal.
     input  logic          reset,  // Reset the module.
@@ -20,11 +19,9 @@ module gcd (
     output logic          ack,    // Input received / Computation is complete.
     output logic [15 : 0] C       // The result.
 );
-    typedef enum logic [1 : 0] {
-     idle,
-     reg_input, 
-     comb_logic
-     } state_t; // Input your own state names here
+    typedef enum logic [3 : 0] {
+        IDLE, READ_A, ACK_A, WAIT_B, READ_B, COMPARE, B_SUB_A, A_SUB_B, DONE
+    } state_t;
 
     shortint unsigned reg_a, next_reg_a, reg_b, next_reg_b;
     
@@ -32,36 +29,87 @@ module gcd (
     
     // Combinatorial logic
     always_comb begin
+        next_state = state;
+        next_reg_a = reg_a;
+        next_reg_b = reg_b;
+        ack = 1'b0;
+        C = 'z;
         case (state)
-            // <COMBINATORIAL BODY> 
-            idle: begin
-                //idle logic
-                if (req == 1) begin
-                    next_state = reg_input;
-                end
+            IDLE: begin
+                ack =0;
+                if (req)
+                    next_state = READ_A;
+                else
+                    next_state = IDLE;
             end
-            
-            reg_input: begin
-                
-            
+
+            READ_A: begin
+                next_reg_a = AB;
+                next_state = ACK_A;
             end
-            
-            default: begin
-                next_state = idle
+
+            ACK_A: begin
+                ack = 1'b1;
+                if(!req)
+                    next_state = WAIT_B;
+                else
+                    next_state = ACK_A;
             end
-            
-          
-            
-            
+
+            WAIT_B: begin
+                ack = 1'b0;
+                if(req)
+                    next_state = READ_B;
+                else
+                    next_state = WAIT_B;
+            end
+
+            READ_B: begin
+                next_reg_b = AB;
+                next_state = COMPARE;
+            end
+
+            COMPARE: begin
+                if(reg_a != reg_b)
+                    if(reg_a < reg_b)
+                        next_state = B_SUB_A;
+                    else
+                        next_state = A_SUB_B;
+                else
+                    next_state = DONE;
+            end
+
+            B_SUB_A: begin
+                next_reg_b = reg_b - reg_a;
+                next_state = COMPARE;
+            end
+
+            A_SUB_B: begin
+                next_reg_a = reg_a - reg_b;
+                next_state = COMPARE;
+            end
+
+            DONE: begin
+                C = reg_a;
+                ack = 1'b1;
+                if(!req)
+                    next_state = IDLE;
+                else
+                    next_state = DONE;
+            end
         endcase
     end
 
         // Register
     always_ff @(posedge clk or posedge reset) begin
-        // <REGISTER BODY>
         if (reset)
-            state <= idle;
+            state_reg <= INIT;
+            reg_a <= '0;
+            reg_b <= '0;
         else
-            state <= next_state;
+            state_reg <= state_next;
+            reg_a <= next_reg_a
+            reg_b <= next_reg_b
     end
+
 endmodule
